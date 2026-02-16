cde4c1d65030b3e1aedfcca90aa25d886e157674
Andrew ONeill <andy@evolve757.com>
2026-02-16 12:28:10 -0500

    edited and completed exercises.

--- app.js
+++ app.js
@@ -2,78 +2,92 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-for(let i = 0; i <= 100; i++) {
-    if (i % 2 == 0) {
-        continue;
-    }else console.log(i);
+for(let i = 1; i <= 100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i)
+    }
 }
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
-for(let i = 0; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FIZZBUZZ");
-    }else{
-        if (i % 5 == 0) {
-            console.log("BUZZ");
-        }
-        if (i % 3 == 0) {
-            console.log("FIZZ");
-        }
+for(let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} FIZZBUZZ`)
+    }else if (i % 3 === 0) {
+        console.log(`${i} FIZZ`)
+    }else if (i % 5 === 0) {
+        console.log(`${i} BUZZ`)
     }
 }
+
 //Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
 let i = 1;
-do {
-    if (i % 2 !== 0) {
-        console.log(i);
-        
+
+while (i <= 100){
+   if (i % 2 !== 0) {
+       console.log(i)
     } i++
-} while (i <= 100);
+} 
+
+let j = 1;
+do {
+    if (j % 2 !== 0) {
+        console.log(j)
+        
+    } j++
+} while (j <= 100);
 
 let x = 1
 do{
-    if (x % 3 == 0 && x % 5 == 0) {
-        console.log("FIZZBUZZ");
-    }
-    else {
-        if (x % 5 == 0) {
-            console.log("BUZZ");
-        }
-        if (x % 3 == 0) {
-            console.log("FIZZ");
-        }
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log(`${x} FIZZBUZZ`)
+    }else if (x % 3 === 0) {
+        console.log(`${x} FIZZ`)
+    }else if (x % 5 === 0) {
+        console.log(`${x} BUZZ`)
     } x++
 } while (x <= 100);
 
+let y = 1
+while (y <= 100) {
+    if (y % 3 === 0 && y % 5 === 0) {
+        console.log(`${y} FIZZBUZZ`)
+    } else if (y % 3 === 0) {
+        console.log(`${y} FIZZ`)
+    } else if (y % 5 === 0) {
+        console.log(`${y} BUZZ`)
+    }y++
+}
+
+//Exercise 4
 console.log("EXERCISE 4:\n==========\n");
 let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
 let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+let found = false;
 for (let i = 1; i <= n; i++) {
-    if (i == value){
+    if (i === value) {
+        found = true;
         console.log("Found Value");
         break;
     }
-    if (i == n){
-        console.log("Did not find Value");
-    }
+}
+if (!found){
+    console.log("Value not found");
 }
 
+//Exercise 5
 console.log("EXERCISE 5:\n==========\n");
-let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
-let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
-let num = Math.round(Math.random() * (1000 - 1) + 1);
-let start = Math.round(Math.random() * (10 - 1) + 1);
-for(let i = start; i <= num; i++) {
-    if (i == fizzDivisor && i == buzzDivisor) {
-        console.log("FIZZBUZZ");
-    }else{
-        if (i == buzzDivisor) {
-            console.log("BUZZ");
-        }
-        if (i == fizzDivisor) {
-            console.log("FIZZ");
-        }
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let start = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let end = Math.round(Math.random() * (1000 - 100) + 100); // creates a random number between 100 and 1000
+for(; start <= end; start++) {
+    if (start % fizzDivisor === 0 && start % buzzDivisor === 0) {
+        console.log(`${start} FIZZBUZZ`);
+    }else if (start % buzzDivisor === 0) {
+        console.log(`${start} BUZZ`);
+    }else if (start % fizzDivisor === 0) {
+        console.log(`${start} FIZZ`);
     }
 }
